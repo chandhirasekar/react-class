ca0565ceddae7eebec642178d31bf0b73a006141
chandhirasekar <chandhirasekar76@gmail.com>
2022-10-15 19:14:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <form>
+      <label>username</label>
+      <input type="text" placehoder="chandhira sekar"/>
+      <label>password</label>
+      <input></input>
+
+    </form>
+  );
+}
+
+export default App;
